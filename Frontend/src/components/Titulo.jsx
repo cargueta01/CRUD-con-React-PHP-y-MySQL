59d2e9ab2ac5a7bb34f07af627d26c5044295fcd
Carlos Argueta <aa20099@ues.edu.sv>
2024-09-14 19:21:44 -0600

    version 6

---
+++ Frontend/src/components/Titulo.jsx
@@ -0,0 +1,16 @@
+//import imgExplosion from "../assets/imgs/explosion.svg";
+
+const Titulo = () => {
+  return (
+    <div className="col-md-12">
+      <h1 className="text-center titulo mb-5">
+         Aplicación de Gestión de Contactos CRUD Full Stack con React, PHP y
+         de
+        MySQL
+        <hr />
+      </h1>
+    </div>
+  );
+};
+
+export default Titulo;
